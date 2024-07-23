bc129edbecdaa593b8fdd4c2695fb8e363ae3b26
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2024-07-23 17:31:18 +0200

    Added build types to builder script

--- builder.sh
+++ builder.sh
@@ -2,4 +2,7 @@
 mkdir build
 cd build
 cmake ../
-cmake --build .
+cmake --build . --target ALL_BUILD --config Release
+cmake --build . --target ALL_BUILD --config Debug
+cmake --build . --target ALL_BUILD --config MinSizeRel
+cmake --build . --target ALL_BUILD --config RelWithDebInfo
