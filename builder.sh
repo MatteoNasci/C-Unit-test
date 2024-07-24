7464c6f9036009e7a52ee34e96a74fa2cd6404d4
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2024-07-24 02:16:04 +0200

    changed building order

--- builder.sh
+++ builder.sh
@@ -3,6 +3,6 @@ mkdir build
 cd build
 cmake ../
 cmake --build . --target ALL_BUILD --config Release
-cmake --build . --target ALL_BUILD --config Debug
 cmake --build . --target ALL_BUILD --config MinSizeRel
 cmake --build . --target ALL_BUILD --config RelWithDebInfo
+cmake --build . --target ALL_BUILD --config Debug
